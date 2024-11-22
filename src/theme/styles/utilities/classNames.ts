295e6f14a090322d37be75b895fefba98477687c
Brandon LaRouche <bjlarouche@verizon.net>
2024-11-22 14:55:46 -0800

    We can check for ConditionalStylesMap looking at key value types

--- src/theme/styles/utilities/classNames.ts
+++ src/theme/styles/utilities/classNames.ts
@@ -30,7 +30,25 @@ function classNames<T extends Instance>(
 		try {
 			const lastArg = args[args.size() - 1];
 
-			return lastArg !== undefined && typeIs(lastArg, "table");
+			// If the last argument is undefined or not a table then its not a ConditionalStyles<T>
+			// Map<K, V> compiles to a table literal (ref: https://roblox-ts.com/docs/api/constructors)
+			if (lastArg === undefined || !typeIs(lastArg, "table")) {
+				return false;
+			}
+
+			const lastArgAsConditionalStylesMap = lastArg as ConditionalStylesMap<T>;
+			let matchesMapFormat = true;
+
+			lastArgAsConditionalStylesMap?.forEach((value, key) => {
+				// Assume that the last argument is a ConditionalStyles<T> with correct format (Map<WriteableStyle<T>, boolean>)
+				// If the key is not a WriteableStyle<T> then it does not match the format
+				// If the value is not a boolean then it does not match the format
+				if (!typeIs(value, "boolean") || !typeIs(key, "table")) {
+					matchesMapFormat = false;
+				}
+			});
+
+			return matchesMapFormat;
 		} catch {
 			// If we catch an error, it means that the last argument is not a ConditionalStyles<T>
 			return false;
@@ -39,14 +57,14 @@ function classNames<T extends Instance>(
 
 	try {
 		if (hasConditionalStyles(args)) {
-		// Handle case with ConditionalStyles
-		const conditionalStyles = args.pop() as ConditionalStylesMap<T>;
-		const styles = args as Array<WriteableStyle<T>>;
+			// Handle case with ConditionalStyles
+			const conditionalStyles = args.pop() as ConditionalStylesMap<T>;
+			const styles = args as Array<WriteableStyle<T>>;
 
-		// ConditionalStyles<T> is the last argument
-		return classNamesInternal(conditionalStyles, ...styles);
-	}
-	} catch  {
+			// ConditionalStyles<T> is the last argument
+			return classNamesInternal(conditionalStyles, ...styles);
+		}
+	} catch {
 		// If we catch an error, like attempt to iterate over a string value, it means that the last argument is not a ConditionalStyles<T>
 		const styles = args as Array<WriteableStyle<T>>;
 		return classNamesInternal(undefined, ...styles);
