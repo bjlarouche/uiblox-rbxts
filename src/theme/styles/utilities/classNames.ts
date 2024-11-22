30e0cbd45d4afe55025af2d97f5d5c8cc285677d
Brandon LaRouche <bjlarouche@verizon.net>
2024-11-22 13:09:14 -0800

    Update classNames to support variadic tuple

--- src/theme/styles/utilities/classNames.ts
+++ src/theme/styles/utilities/classNames.ts
@@ -1,3 +1,4 @@
+import { func } from "@rbxts/react/src/prop-types";
 import { WriteableStyle } from "../types/WriteableStyle";
 
 export type GenericStyle = {
@@ -7,11 +8,10 @@ export type GenericStyle = {
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type ConditionalStyles<T extends Instance> = Map<WriteableStyle<T>, boolean>;
 
-const classNames = <T extends Instance>(
+const classNamesInternal = <T extends Instance>(
 	conditionalStyles?: ConditionalStyles<T>,
 	...styles: Array<WriteableStyle<T>>
 ): WriteableStyle<T> => {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	let style = {} as WriteableStyle<T>;
 
 	conditionalStyles?.forEach((value, key) => {
@@ -28,5 +28,28 @@ const classNames = <T extends Instance>(
 						
 	return { ...style };
 };
+
+// Use variadic tuple types to allow for multiple WriteableStyle<T> arguments with an optional ConditionalStyles<T> argument at the end
+function classNames<T extends Instance>(...args: [...Array<WriteableStyle<T>>, ConditionalStyles<T>] | [...Array<WriteableStyle<T>>]): WriteableStyle<T> {
+    // Check if last argument is ConditionalStyles
+    const hasConditionalStyles = (args: any[]): args is [...Array<WriteableStyle<T>>, ConditionalStyles<T>] => {
+        const lastArg = args.pop();
+        return lastArg !== undefined && lastArg instanceof Map; // If it's an array, it should have sequential numeric indices
+    };
+
+    if (hasConditionalStyles(args)) {
+        // Handle case with ConditionalStyles
+        const conditionalStyles = args.pop() as ConditionalStyles<T>;
+        const styles = args as Array<WriteableStyle<T>>;
+		
+		// ConditionalStyles<T> is the last argument
+		return classNamesInternal(conditionalStyles, ...styles);
+	}
+	
+		// Handle case without ConditionalStyles
+	// All args are WriteableStyle<T>
+	const styles = args as Array<WriteableStyle<T>>;
+	return classNamesInternal(undefined, ...styles);
+}
 
 export default classNames;
