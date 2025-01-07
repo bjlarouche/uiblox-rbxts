d92008cf0d879c5ad3c207582ac1fb77c8c314ff
Brandon LaRouche <bjlarouche@verizon.net>
2025-01-06 16:46:22 -0800

    Update Divider.tsx

--- src/ui/packages/divider/components/Divider.tsx
+++ src/ui/packages/divider/components/Divider.tsx
@@ -12,7 +12,7 @@ export interface DividerProps {
 	color?: Color3;
 	transparency?: number;
 	weight?: number;
-	anchorPoint: Vector2;
+	anchorPoint?: Vector2;
 }
 
 function Divider<T extends DefaultDividerComponent>(props: CustomizedProps<T, DividerProps>) {
